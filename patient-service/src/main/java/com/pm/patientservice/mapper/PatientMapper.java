3558bb2e7e3df8fdaa08fb57d9dd68b8491dc8e5
Samarth <tikotkarsamarth@gmail.com>
2025-06-02 19:50:49 +0530

    CRUD functionalities and API docs

---
+++ patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
@@ -0,0 +1,30 @@
+package com.pm.patientservice.mapper;
+
+import com.pm.patientservice.dto.PatientRequestDTO;
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.model.Patient;
+
+import java.time.LocalDate;
+
+public class PatientMapper {
+    public static PatientResponseDTO toDTO(Patient patient) {
+        PatientResponseDTO patientDTO = new PatientResponseDTO();
+        patientDTO.setId(patient.getId().toString());       // Convert UUID to String
+        patientDTO.setName(patient.getName());             // Copy name directly
+        patientDTO.setAddress(patient.getAddress());       // Copy address
+        patientDTO.setEmail(patient.getEmail());           // Copy email
+        patientDTO.setDateOfBirth(patient.getDateOfBirth().toString()); // Format date
+
+        return patientDTO;
+    }
+
+    public static Patient toModel(PatientRequestDTO patientRequestDTO) {
+        Patient patient = new Patient();
+        patient.setName(patientRequestDTO.getName());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+        patient.setRegisteredDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()));
+        return patient;
+    }
+}
